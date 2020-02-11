009d6debac13e6f6dcaa46fc36ceee2797aeaf57
Maaz Bin Tausif <43075856+maaztausif@users.noreply.github.com>
2020-02-11 06:13:44 +0500

    Complete App

---
+++ I_Am_Rich/I Am RIch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am RIch
+//
+//  Created by maJor on 08/02/2020.
+//  Copyright © 2020 maJor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
